ac8ac1e8d6ad5cbecad21efedc438ad796afb666
gurjarkunal <kunalgurjarkg786@gmail.com>
2024-04-08 01:34:15 +0530

    Initial

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { TfiMouse } from "react-icons/tfi";
 import "./Home.css";
 import Product from "./Product";
@@ -16,28 +16,32 @@ function Home() {
   }, [dispatch]);
 
   return (
-    <>
-      <MetaData title="KharidLo" />
-      <div className="banner">
-        <p>WelCome to Kharidlo</p>
-        <h1>FIND AMAZING PRODUCTS BELOW </h1>
-        <a href="#container">
-          <button>
-            Scroll <TfiMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        {products &&
-          products.map((products, key) =>{
-            // console.log(products);
-            return(
-              <Product key={key} products={products} />
-            )
-          })}
-      </div>
-    </>
+    <Fragment>
+      {loading ? (
+        "loading"
+      ) : (
+        <Fragment>
+          <MetaData title="KharidLo" />
+          <div className="banner">
+            <p>WelCome to Kharidlo</p>
+            <h1>FIND AMAZING PRODUCTS BELOW </h1>
+            <a href="#container">
+              <button>
+                Scroll <TfiMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((products, key) => {
+                // console.log(products);
+                return <Product key={key} products={products} />;
+              })}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 }
 
